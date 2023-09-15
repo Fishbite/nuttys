5bd2ad8f9bffa3f8663d9a79f781b8c9379296bc
Fishbite <43952420+Fishbite@users.noreply.github.com>
2023-09-15 17:36:55 +0100

    Created component class `MakeFooter` can be used to create a footer on each page dynamically

---
+++ src/components.js
@@ -0,0 +1,20 @@
+class MakeFooter {
+  constructor(email, list, ip) {
+    this.email = email;
+    this.ip = ip;
+    this.list = list;
+  }
+  render() {
+    return `<p>${this.email}</p> <ul>${this.list}</ul><p>${this.ip}</p>`;
+  }
+}
+
+const footerEmail = "kitchen4nutty@gmail.com";
+const footerList = `<li><a href="#">test list</a></li><li><a href="#">test list</a></li><li><a href="#">test list</a></li>`;
+const footerIp = `copyright: fishbite 2023`;
+
+const footerContent = new MakeFooter(footerEmail, footerList, footerIp);
+
+const footer = document.createElement("footer");
+document.body.appendChild(footer);
+footer.innerHTML = footerContent.render();
