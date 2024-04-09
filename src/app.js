85b268553867d6702dbd49bb49610a6ac2478585
Fishbite <43952420+Fishbite@users.noreply.github.com>
2024-04-09 19:48:51 +0100

    menu text changed

--- src/app.js
+++ src/app.js
@@ -135,7 +135,7 @@ const navContent = {
   "recipepage.html#beef": "Beef Bangin'",
   "recipepage.html#pork": "Pork Packin'",
   "recipepage.html#lamb": "Lamb Huggin'",
-  "recipepage.html#fish & veg": "Fish 'n' Veg' Bakin'",
+  "recipepage.html#fish & veg": "Fish & Veg' Bakin'",
   "recipepage.html#sauce": "Saucey Basics",
   "recipepage.html#accompaniments": "Accompaniments",
   "recipepage.html#puddings": "Puddin' Passion",
