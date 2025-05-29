d5d9de5acf41046e727602fdf5e40f256b4cab6a
Fishbite <43952420+Fishbite@users.noreply.github.com>
2025-05-29 14:41:24 +0100

    temp redirect of recipe page whilst updating

--- src/app.js
+++ src/app.js
@@ -237,7 +237,8 @@ class RecipePageRedirector {
 
 // Usage
 new RecipePageRedirector({
-  dynamicURL: "https://recipes.nuttyskitchen.co.uk/recipes",
+  // dynamicURL: "https://recipes.nuttyskitchen.co.uk/recipes",
+  dynamicURL: "https://www.nuttyskitchen.co.uk/recipepage.html",
   staticURL: "https://www.nuttyskitchen.co.uk/recipepage.html",
   selector: "a.recipe-page-link",
   testImage: "https://recipes.nuttyskitchen.co.uk/images/icons/blank.svg",
